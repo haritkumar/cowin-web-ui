8be29f8a8ca4fb9a06ce4e6badc75163961c6163
Harit Kumar <harit.kumar01@nagarro.com>
2021-05-14 15:39:23 +0530

    find by pincode

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,19 +8,35 @@ import * as SockJS from 'sockjs-client';
 })
 export class HomeComponent implements OnInit {
   stompClient: any;
+  disabled = true;
+  sessionsMap = new Map();  
   constructor() { }
+
+  setConnected(connected: boolean) {
+    this.disabled = !connected;
+  }
 
   ngOnInit(): void {
     console.log("Initialize WebSocket Connection");
     const socket = new SockJS('http://localhost:8080/stomp-endpoint-ws');
     this.stompClient = Stomp.over(socket);
-
+    this.stompClient.debug = null
   const _this = this;
   this.stompClient.connect({}, function (frame) {
-    _this.stompClient.subscribe('/topic/slots', function (payload) {
-      console.log(payload);
+    _this.stompClient.subscribe('/topic/slotByPin', function (payload) {
+    JSON.parse(payload.body).sessions.forEach( (session) => {
+       _this.sessionsMap.set(session.date+'--'+session.center_id+'--'+session.pincode, session);  
+    });
     });
   });
+  }
+
+  ngOnDestroy() {
+    if (this.stompClient != null) {
+      this.stompClient.disconnect();
+    }
+    this.setConnected(false);
+    alert('Disconnected!');
   }
 
 }
