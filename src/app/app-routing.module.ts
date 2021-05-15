a15c462ca598b36260d0af0f4b3bdaee8e12b24b
Harit Kumar <harit.kumar01@nagarro.com>
2021-05-15 22:15:22 +0530

    removing stomp websocket

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {InnerLayoutComponent} from './layout/inner-layout/inner-layout.component';
 import {OpenLayoutComponent} from './layout/open-layout/open-layout.component';
-import {HomeComponent} from './components/home/home.component';
 import {NotFoundComponent} from './components/not-found/not-found.component';
 import {CalendarPinComponent} from './components/calendar-pin/calendar-pin.component';
 const routes: Routes = [
@@ -10,7 +9,6 @@ const routes: Routes = [
     path: '', 
     component: InnerLayoutComponent,
     children: [
-      { path: 'home', component: HomeComponent},
       { path: 'calendarByPincode', component: CalendarPinComponent},
       { path: '', redirectTo: 'calendarByPincode', pathMatch: 'full'},
     ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './components/home/home.component';
 import { InnerLayoutComponent } from './layout/inner-layout/inner-layout.component';
 import { OpenLayoutComponent } from './layout/open-layout/open-layout.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
@@ -16,7 +15,6 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     InnerLayoutComponent,
     OpenLayoutComponent,
     NotFoundComponent,
